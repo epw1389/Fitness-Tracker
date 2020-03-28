aa4b59f7d743181c31dddb3b7c40f47b26dd02d7
Lisa Walter <epw1389@gmail.com>
2020-03-28 13:17:34 -0400

    update css

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    workout_name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    exercises: [{
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
+    }],
+    created: {
+        type: Date
+    }
+});
+const exerciseSchema = new Schema({
+    exercise_name: {
+        type: String,
+        required: true
+    },
+    num_of_reps: {
+        type: Number,
+        required: true
+    }
+});
+const Workout = mongoose.model("Workout", workoutSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+module.exports = {Workout, Exercise};
